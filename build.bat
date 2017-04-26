288cfa7f634b63097c8cde17ff48f66a5a07f876
Ruben Aalders <rubenaalders@hotmail.com>
2017-04-26 16:58:14 +0200

    Changed default MSbuild to visual studio DevCMD

--- build.bat
+++ build.bat
@@ -6,8 +6,13 @@ echo Start SLN build
 "C:\program files\Unity\Editor\Unity.exe" -projectPath "%~dp0\HoloLens Mapping and Movement" -logFile "%~dp0\log.txt" -buildOutput "%~dp0\HoloLens Mapping and Movement\App" -duskBuildTarget WSAPlayer -wsaSDK UWP -wsaUWPBuildType D3D -executeMethod HoloToolkit.Unity.BuildSLNUtilities.PerformBuild_CommandLine -batchmode -quit
 
 echo.
+echo Restoring NuGet packages
+nuget restore "%~dp0\HoloLens Mapping and Movement\UWP\project.json"
+
+echo.
 echo Start APPX build
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /nologo /verbosity:d "/target:Build" /toolsversion:14.0 /p:Configuration=Debug /p:Platform="x86" /p:OutDir=bin /p:GenerateProjectSpecificOutputFolder=true /p:StyleCopTreatErrorsAsWarnings=false /maxcpucount "%~dp0\HoloLens Mapping and Movement\App\HoloLens Mapping and Movement.sln"
+call "%vs140comnTools%\VsDevCmd.bat"
+MSBuild.exe /nologo /verbosity:d "/target:Build" /p:Configuration=Debug /p:Platform="x86" /p:OutDir=bin /p:GenerateProjectSpecificOutputFolder=true /p:StyleCopTreatErrorsAsWarnings=false /maxcpucount "%~dp0\HoloLens Mapping and Movement\App\HoloLens Mapping and Movement.sln"
 
 echo.
 echo Done
