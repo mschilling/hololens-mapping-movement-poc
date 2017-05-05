53960a482fd6ef3f8ac8d389e9cd8b896a314dd9
Ruben Aalders <rubenaalders@hotmail.com>
2017-05-05 08:39:26 +0200

    Implemented Spatial Planes

---
+++ HoloLens Mapping and Movement/Assets/Script/PlaySpaceManager.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using HoloToolkit.Unity.SpatialMapping;
+using HoloToolkit.Unity;
+
+/// <summary>
+/// The SurfaceManager class allows applications to scan the environment for a specified amount of time 
+/// and then process the Spatial Mapping Mesh (find planes, remove vertices) after that time has expired.
+/// </summary>
+public class PlaySpaceManager : Singleton<PlaySpaceManager>
+{
+
+    [Tooltip("Every N (in seconds) the mesh will update.")]
+    public float updateTime = 5;
+
+    [Tooltip("Material to use when rendering Spatial Mapping meshes while the observer is running.")]
+    public Material defaultMaterial;
+
+    [Tooltip("Optional Material to use when rendering Spatial Mapping meshes after the observer has been stopped.")]
+    public Material secondaryMaterial;
+
+    [Tooltip("Minimum number of floor planes required in order to exit scanning/processing mode.")]
+    public uint minimumFloors = 1;
+
+    [Tooltip("Minimum number of wall planes required in order to exit scanning/processing mode.")]
+    public uint minimumWalls = 1;
+
+    /// <summary>
+    /// Indicates if processing of the surface meshes is complete.
+    /// </summary>
+    private bool meshesProcessed = false;
+
+    /// <summary>
+    /// GameObject initialization.
+    /// </summary>
+    private void Start()
+    {
+        // Update surfaceObserver and storedMeshes to use the same material during scanning.
+        SpatialMappingManager.Instance.SetSurfaceMaterial(defaultMaterial);
+
+        // Register for the MakePlanesComplete event.
+        SurfaceMeshesToPlanes.Instance.MakePlanesComplete += SurfaceMeshesToPlanes_MakePlanesComplete;
+
+        InvokeRepeating("CreatePlanes", updateTime, updateTime);
+    }
+
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
+    private void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Handler for the SurfaceMeshesToPlanes MakePlanesComplete event.
+    /// </summary>
+    /// <param name="source">Source of the event.</param>
+    /// <param name="args">Args for the event.</param>
+    private void SurfaceMeshesToPlanes_MakePlanesComplete(object source, System.EventArgs args)
+    {
+        /* TODO: 3.a DEVELOPER CODING EXERCISE 3.a */
+
+        // Collection of floor and table planes that we can use to set horizontal items on.
+        List<GameObject> horizontal = new List<GameObject>();
+
+        // Collection of wall planes that we can use to set vertical items on.
+        List<GameObject> vertical = new List<GameObject>();
+
+        // 3.a: Get all floor and table planes by calling
+        // SurfaceMeshesToPlanes.Instance.GetActivePlanes().
+        // Assign the result to the 'horizontal' list.
+        horizontal = SurfaceMeshesToPlanes.Instance.GetActivePlanes(PlaneTypes.Table | PlaneTypes.Floor);
+
+        // 3.a: Get all wall planes by calling
+        // SurfaceMeshesToPlanes.Instance.GetActivePlanes().
+        // Assign the result to the 'vertical' list.
+        vertical = SurfaceMeshesToPlanes.Instance.GetActivePlanes(PlaneTypes.Wall);
+
+        // Check to see if we have enough horizontal planes (minimumFloors)
+        // and vertical planes (minimumWalls), to set holograms on in the world.
+        if (horizontal.Count >= minimumFloors && vertical.Count >= minimumWalls)
+        {
+            // We have enough floors and walls to place our holograms on...
+
+            // 3.a: Let's reduce our triangle count by removing triangles
+            // from SpatialMapping meshes that intersect with our active planes.
+            // Call RemoveVertices().
+            // Pass in all activePlanes found by SurfaceMeshesToPlanes.Instance.
+            RemoveVertices(SurfaceMeshesToPlanes.Instance.ActivePlanes);
+
+            // 3.a: We can indicate to the user that scanning is over by
+            // changing the material applied to the Spatial Mapping meshes.
+            // Call SpatialMappingManager.Instance.SetSurfaceMaterial().
+            // Pass in the secondaryMaterial.
+            SpatialMappingManager.Instance.SetSurfaceMaterial(secondaryMaterial);
+
+            // 3.a: We are all done processing the mesh, so we can now
+            // initialize a collection of Placeable holograms in the world
+            // and use horizontal/vertical planes to set their starting positions.
+            // Call SpaceCollectionManager.Instance.GenerateItemsInWorld().
+            // Pass in the lists of horizontal and vertical planes that we found earlier.
+            //SpaceCollectionManager.Instance.GenerateItemsInWorld(horizontal, vertical);
+        }
+        else
+        {
+            // We do not have enough floors/walls to place our holograms on...
+
+            // 3.a: Re-enter scanning mode so the user can find more surfaces by 
+            // calling StartObserver() on the SpatialMappingManager.Instance.
+            SpatialMappingManager.Instance.StartObserver();
+
+            // 3.a: Re-process spatial data after scanning completes by
+            // re-setting meshesProcessed to false.
+            meshesProcessed = false;
+        }
+    }
+
+    /// <summary>
+    /// Creates planes from the spatial mapping surfaces.
+    /// </summary>
+    private void CreatePlanes()
+    {
+        // Generate planes based on the spatial map.
+        SurfaceMeshesToPlanes surfaceToPlanes = SurfaceMeshesToPlanes.Instance;
+        if (surfaceToPlanes != null && surfaceToPlanes.enabled)
+        {
+            surfaceToPlanes.MakePlanes();
+        }
+    }
+
+    /// <summary>
+    /// Removes triangles from the spatial mapping surfaces.
+    /// </summary>
+    /// <param name="boundingObjects"></param>
+    private void RemoveVertices(IEnumerable<GameObject> boundingObjects)
+    {
+        RemoveSurfaceVertices removeVerts = RemoveSurfaceVertices.Instance;
+        if (removeVerts != null && removeVerts.enabled)
+        {
+            removeVerts.RemoveSurfaceVerticesWithinBounds(boundingObjects);
+        }
+    }
+
+    /// <summary>
+    /// Called when the GameObject is unloaded.
+    /// </summary>
+    private void OnDestroy()
+    {
+        if (SurfaceMeshesToPlanes.Instance != null)
+        {
+            SurfaceMeshesToPlanes.Instance.MakePlanesComplete -= SurfaceMeshesToPlanes_MakePlanesComplete;
+        }
+    }
+}
