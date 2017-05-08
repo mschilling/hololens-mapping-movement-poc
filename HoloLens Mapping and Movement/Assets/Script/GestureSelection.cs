3b8a43297f39f48d38d0e51dd24e6eaab79e1236
Ruben Aalders <rubenaalders@hotmail.com>
2017-05-08 15:20:26 +0200

    Added Global ClickListener

---
+++ HoloLens Mapping and Movement/Assets/Script/GestureSelection.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GestureSelection : MonoBehaviour, IInputClickHandler {
+
+    public InputManager input;
+
+    // Use this for initialization
+    void Start () {
+        input.AddGlobalListener(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Clicked!");
+    }
+
+}
