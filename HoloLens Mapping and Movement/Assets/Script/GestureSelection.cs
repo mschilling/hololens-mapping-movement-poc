d00339dd3122bf5340d4def8656a5d2821a0e938
Ruben Aalders <rubenaalders@hotmail.com>
2017-06-07 11:16:32 +0200

    Added descriptive comments to GestureSelection

--- HoloLens Mapping and Movement/Assets/Script/GestureSelection.cs
+++ HoloLens Mapping and Movement/Assets/Script/GestureSelection.cs
@@ -24,15 +24,18 @@ public class GestureSelection : MonoBehaviour, IInputClickHandler {
 	
 	// Update is called once per frame
 	void Update () {
+        // As long as the user has not finished scanning do nothing in Update
         if (!playSpaceManager.finishedScanning())
             return;
-
+        
+        // If player variable is null add player object to variable
         if (player == null)
         {
             Debug.Log("Looking for player object!");
 
             player = GameObject.FindGameObjectWithTag("Player");
 
+            // When player is found also add the move script to a variable for easy access
             if(player != null)
             {
                 Debug.Log("Found player object!");
@@ -43,33 +46,48 @@ public class GestureSelection : MonoBehaviour, IInputClickHandler {
         }
     }
 
+    /// <summary>
+    /// Global input listener
+    /// Listen for every click performed by user
+    /// </summary>
+    /// <param name="eventData">Input event data</param>
     public void OnInputClicked(InputClickedEventData eventData)
     {
         Debug.Log("Clicked!");
 
+        // Room has not been finished
         if (!playSpaceManager.finishedScanning())
         {
             playSpaceManager.finishScanning();
         }
+        // Room has been finished, check where user has clicked
         else
         {
             CastRayToWorld();
         }
     }
 
+    /// <summary>
+    /// Call player object reset
+    /// </summary>
     public void ResetPlayer()
     {
         Debug.Log("Reset");
 
-        if (playSpaceManager.finishedScanning())
+        // Only perform the reset if reset can be called
+        if (playerMove != null)
         {
             CallPlayer();
             playerMove.Reset();
         }
     }
 
+    /// <summary>
+    /// Cast a raycast to get the location where the user was gazing at when he performed a click
+    /// </summary>
     public void CastRayToWorld()
     {
+        // If raycast hits an object it returns true
         RaycastHit hitInfo;
         if (Physics.Raycast(
                 Camera.main.transform.position,
@@ -83,10 +101,14 @@ public class GestureSelection : MonoBehaviour, IInputClickHandler {
             // Do something with the object that was hit by the raycast.
             Debug.Log("World Location: " + hitInfo.point.ToString("F4"));
 
+            // Pass the location of the gaze location when clicked to the player object
             playerMove.MoveToLocation(hitInfo.collider.gameObject, hitPoint);
         }
     }
 
+    /// <summary>
+    /// Show user feedback when calling player object
+    /// </summary>
     private void CallPlayer()
     {
         TextManager.Instance.LetCatSpeak("Ik kom eraan!");
