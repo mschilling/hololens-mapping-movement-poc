a85ca6d66d266e286204d796732b1611ed8294cf
Ruben Aalders <rubenaalders@hotmail.com>
2017-06-28 16:58:48 +0200

    Setup unit test

---
+++ HoloLens Mapping and Movement/Assets/Editor/TestMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class TestMove {
+
+	[Test]
+	public void EditorTest() {
+        InitializeScene();
+
+        //Arrange
+        var gameObject = GameObject.FindGameObjectWithTag("Player");
+
+		//Act
+		//Try to rename the GameObject
+		var newGameObjectName = "My game object";
+		gameObject.name = newGameObjectName;
+
+		//Assert
+		//The object has a new name
+		Assert.AreEqual(newGameObjectName, gameObject.name);
+	}
+
+    private void InitializeScene()
+    {
+        PlaySpaceManager playSpaceManager = GameObject.FindGameObjectWithTag("Spatial Processing").GetComponent<PlaySpaceManager>();
+
+        playSpaceManager.finishScanning();
+
+        while(!playSpaceManager.finishedScanning())
+        {
+            // Wait..
+        }
+    }
+}
